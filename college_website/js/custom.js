c0ca9bcce2fc9abdf1c3b3e42456d1f632cf127f
Ankita_Ramugade <79557346+ankita-ramugade@users.noreply.github.com>
2022-02-15 16:06:51 +0530

    Delete college_website directory

--- college_website/js/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('.chat_icon').click(function(event) {
-		$('.chat_box').toggleClass('active');
-	});
-
-	$('.conv-from-wrapper').convfrom({selectInputStyle: 'disable'});
-})
